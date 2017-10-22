d01bf181833ed6a8b8b500069f0db0d70dd64cb1
danizaghian <dani.zaghian@gmail.com>
2017-10-22 09:22:54 -0700

    api meetings index

---
+++ app/controllers/api/v1/meetings_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::MeetingsController < ApplicationController
+
+  def index
+    @meetings = Meeting.all
+    render 'index.json.jbuilder'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   get '/meetings' => 'meetings#index'
   get '/meetings/:id' => 'meetings#show'
+
+  namespace :api do
+    namespace :v1 do
+      get '/meetings' => 'meetings#index'
+    end
+  end
+
 end
