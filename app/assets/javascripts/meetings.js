78a9f06d78ca2f7f79f4232b748b086a57e5e827
danizaghian <dani.zaghian@gmail.com>
2017-10-22 12:04:44 -0700

    tag filter logic

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -4,23 +4,80 @@ document.addEventListener("DOMContentLoaded", function(event) {
     el: '#app',
     data: {
       meetings: [],
-      nameFilter: ''
+      tags: [],
+      nameFilter: '',
+      orderAttribute: 'startTime',
+      orderAscending: true,
+      tagFilterId: null
     },
     mounted: function() {
       $.get('/api/v1/meetings.json', function(result) {
         this.meetings = result;
       }.bind(this));
+      $.get('/api/v1/tags.json', function(result) {
+        this.tags = result;
+      }.bind(this));
     },
     methods: {
-
+      changeOrderAttribute: function(attribute) {
+        if (attribute !== this.orderAttribute) {
+          this.orderAscending = true;
+        } else {
+          this.orderAscending = !this.orderAscending;
+        }
+        this.orderAttribute = attribute;
+      },
+      filterByTag: function(tag) {
+        this.tagFilterId = tag.id;
+      },
+      resetTagFilter: function() {
+        this.tagFilterId = null;
+      }
     },
     computed: {
       filteredMeetings: function() {
-        return this.meetings.filter(function(meeting) {
+        var filtered = this.meetings.filter(function(meeting) {
           //if nameFilter is in meeting.name
-          return meeting.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
+          var validName = meeting.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
+
+          if (this.tagFilterId) {
+            var validTag = false;
+            for (var i = 0; i < meeting.tags.length; i++) {
+              if(meeting.tags[i].id === this.tagFilterId) {
+                validTag = true;
+              }
+            } 
+          } else {
+            var validTag = true;
+          }
+          return validName && validTag;
+
+        }.bind(this));
+        return filtered.sort(function(meeting1, meeting2) {
+          if (this.orderAscending) {
+            return meeting1[this.orderAttribute] > meeting2[this.orderAttribute];
+          } else {
+            return meeting1[this.orderAttribute] < meeting2[this.orderAttribute];
+          }
         }.bind(this));
       }
     }
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
